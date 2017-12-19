5da34782cd31e0c18b47771f1bad3bf623686242
Inbar Pe'er <inbarpeer@Inbars-MacBook-Air.local>
2017-12-19 11:43:21 -0500

    started

--- forking_server.c
+++ forking_server.c
@@ -12,10 +12,20 @@ static void sighandler(int signo) {
 }
 
 int main() {
+  signal(SIGINT, signalhandler);
+  int to_client, from_client;
+  char buffer[BUFFER_SIZE];
+  while(1){
+    from_client = server_handshake(&to_client);
+    while(read(from_client, buffer, sizeof(buffer)));
+    process(buffer);
+    write(to_client, bugger, sizeof(buffer));
 }
-
+  //gotta add the fork in!!
+  
 void subserver(int from_client) {
 }
 
 void process(char * s) {
+  
 }
